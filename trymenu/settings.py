294604aebaa2a239b1fe71b0e203db419d184559
Nikakwa <nicolasazzi00@hotmail.com>
2020-05-15 16:08:34 +0300

    fixed style of admin

--- trymenu/settings.py
+++ trymenu/settings.py
@@ -144,6 +144,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_ROOT = "https://trymenu.herokuapp.com/static/"
-
 
