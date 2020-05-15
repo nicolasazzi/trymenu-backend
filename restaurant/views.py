09a2612c7d37a1708b18a6670080bc6043bca0a4
Nikakwa <nicolasazzi00@hotmail.com>
2020-05-15 21:08:25 +0300

    fixed bug

--- restaurant/views.py
+++ restaurant/views.py
@@ -14,72 +14,65 @@ from .models import Restaurant
 @permission_classes([IsAuthenticated])
 def restaurants_request_view(request):
 
-
-    if request.GET:
-        
-        try:
-            limit = int(request.GET['limit'])
-        except:
-            limit = 20
-        
-        try:
-            offset = int(request.GET['offset'])
-        except:
-            offset = 0
+    try:
+        limit = int(request.GET['limit'])
+    except:
+        limit = 20
+    
+    try:
+        offset = int(request.GET['offset'])
+    except:
+        offset = 0
 
 
-        restaurants = Restaurant.objects.all()[offset:limit]
-        restaurant_data = []
-        restaurant_counter = 0
+    restaurants = Restaurant.objects.all()[offset:limit]
+    restaurant_data = []
+    restaurant_counter = 0
 
-        for restaurant in restaurants:
+    for restaurant in restaurants:
 
-            categories = restaurant.categories.all()
-            categories_data = []
-            category_counter = 0
+        categories = restaurant.categories.all()
+        categories_data = []
+        category_counter = 0
 
-            for category in categories:
+        for category in categories:
 
-                items = category.items.filter(restaurant=restaurant)
-                item_counter = 0
-                item_data = []
+            items = category.items.filter(restaurant=restaurant)
+            item_counter = 0
+            item_data = []
 
-                for item in items:
+            for item in items:
 
-                    item_serializer = ItemSerializer(item)
+                item_serializer = ItemSerializer(item)
 
-                    try:
-                        did_try = item.item_user_relation_set.get(account=request.user).did_try
-                    except:
-                        did_try = False
+                try:
+                    did_try = item.item_user_relation_set.get(account=request.user).did_try
+                except:
+                    did_try = False
 
-                    item_data.append(item_serializer.data)
-                    item_data[item_counter]['did_try'] = did_try
-                    item_counter += 1
+                item_data.append(item_serializer.data)
+                item_data[item_counter]['did_try'] = did_try
+                item_counter += 1
 
-                category_serializer = CategorySerializer(category)
+            category_serializer = CategorySerializer(category)
 
-                categories_data.append(category_serializer.data)
-                categories_data[category_counter]['category_items'] = item_data
+            categories_data.append(category_serializer.data)
+            categories_data[category_counter]['category_items'] = item_data
 
-                category_counter += 1
-                
-
-            restaurant_serializer = RestaurantSerializer(restaurant)
+            category_counter += 1
             
-            restaurant_data.append(restaurant_serializer.data)
-            restaurant_data[restaurant_counter]['menu'] = categories_data
-            restaurant_counter += 1
 
-        offset +=  limit
-        print(limit)
-        data = {
-            'restaurants' : restaurant_data,
-            'next' : 'https://trymenu.herokuapp.com/api/restaurant/get_restaurants?limit=' + str(limit) + '&offset=' + str(offset)
-        }
+        restaurant_serializer = RestaurantSerializer(restaurant)
+        
+        restaurant_data.append(restaurant_serializer.data)
+        restaurant_data[restaurant_counter]['menu'] = categories_data
+        restaurant_counter += 1
 
-        return Response(data)
+    offset +=  limit
+    print(limit)
+    data = {
+        'restaurants' : restaurant_data,
+        'next' : 'https://trymenu.herokuapp.com/api/restaurant/get_restaurants?limit=' + str(limit) + '&offset=' + str(offset)
+    }
 
-    else:
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
- 
+    return Response(data)
